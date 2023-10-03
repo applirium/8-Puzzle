600bb0724827f817549d4b82e564213a8e48eead
Lukas Stefancik <xtefancik@gstuba.sk>
2023-10-03 15:07:25 +0200

    1.1 akcie done

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+from random import shuffle
+from copy import deepcopy
+
+
 class Node:
-    def __init__(self, table, parent):
+    def __init__(self, table, parent, depth=1):
         self.right = None
         self.left = None
         self.up = None
         self.down = None
-        self.depth = 1
+        self.depth = depth
         self.table = table
         self.parent = parent
 
@@ -16,36 +20,57 @@ class Node:
             node = node.parent
         return path
 
-    def right(self):
-        new_node = Node(self, self.depth)
-        return new_node
+    def actions(self):
+        b = []
 
-    def left(self):
-        new_node = Node(self, self.depth)
-        return new_node
+        for row in self.table:
+            for i in row:
+                if i == "B":
+                    b = [self.table.index(row), row.index(i)]
 
-    def up(self):
-        new_node = Node(self, self.depth)
-        if "B" not in self.table[0]:
-            print(1)
+        if b[1] != len(self.table) - 1:
+            new_table = deepcopy(self.table)
+            new_table[b[0]][b[1]] = new_table[b[0]][(b[1] + 1)]
+            new_table[b[0]][(b[1] + 1)] = "B"
+            self.right = Node(new_table, self, self.depth + 1)
 
-        return new_node
+        if b[1] != 0:
+            new_table = deepcopy(self.table)
+            new_table[b[0]][b[1]] = new_table[b[0]][(b[1] - 1)]
+            new_table[b[0]][(b[1] - 1)] = "B"
+            self.left = Node(new_table, self, self.depth + 1)
 
-    def down(self):
-        new_node = Node(self, self.depth)
-        return new_node
+        if b[0] != 0:
+            new_table = deepcopy(self.table)
+            new_table[b[0]][b[1]] = new_table[(b[0] - 1)][b[1]]
+            new_table[(b[0] - 1)][b[1]] = "B"
+            self.up = Node(new_table, self, self.depth + 1)
+
+        if b[0] != len(self.table[0]) - 1:
+            new_table = deepcopy(self.table)
+            new_table[b[0]][b[1]] = new_table[(b[0] + 1)][b[1]]
+            new_table[(b[0] + 1)][b[1]] = "B"
+            self.down = Node(new_table, self, self.depth + 1)
 
 
-def table_gen(n, m):
+def table_gen(n, m, random=True):
+    temp = []
     node = []
-    index = 1
+    index = 0
+    for i in range(1, m * n + 1):
+        temp.append(i)
+        if i == m * n:
+            temp[-1] = "B"
+
+    if random:
+        shuffle(temp)
+
     for i in range(n):
         inside_node = []
         for j in range(m):
-            inside_node.append(index)
+            inside_node.append(temp[index])
             index += 1
         node.append(inside_node)
-    node[-1][-1] = "B"
     return node
 
 
@@ -59,22 +84,27 @@ def ciferny_maximum(number):
         return 1
 
 
-def print_node(node, scale):
+def print_node(node):
     n = len(node.table)
     m = len(node.table[0])
-    cif_max = ciferny_maximum(n*m)
+    cif_max = ciferny_maximum(n * m)
 
     for i in range(n):
-        print((("-" * (scale + cif_max)) * m) + "-")
-        print((("|" + " " * (scale + cif_max - 1)) * m) + "|")
+        print((("-" * (5 + cif_max)) * m) + "-")
+        print((("|" + " " * (5 + cif_max - 1)) * m) + "|")
         for j in range(m):
             cif_local = ciferny_maximum(node.table[i][j])
-            print(("|" + " " * (scale // 2) + str(node.table[i][j])) + " " * (scale // 2 + cif_max - cif_local), end="")
+            print(("|  " + str(node.table[i][j])) + " " * (2 + cif_max - cif_local), end="")
         print("|")
-        print((("|" + " " * (scale + cif_max - 1)) * m) + "|")
-    print((("_" * (scale + cif_max)) * m) + "_")
+        print((("|" + " " * (5 + cif_max - 1)) * m) + "|")
+    print((("_" * (5 + cif_max)) * m) + "_")
 
 
 stav = Node(table_gen(3, 3), None)
-print(stav.table)
-print_node(stav, 5)
+final_stav = Node(table_gen(3, 3, False), None)
+
+print_node(stav)
+stav.actions()
+final_stav.actions()
+
+print(1)
